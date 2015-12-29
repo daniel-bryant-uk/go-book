9a5f4e7c66a60448d086aa2284b9da3fdad92ae1
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2015-12-29 11:03:07 +0000

    Initial work on Chapter 1

---
+++ chap1/main.go
@@ -0,0 +1,57 @@
+// print command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	//1
+	s, sep := "", ""
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+
+	fmt.Printf("Loop time -> %.2fs elapse\n", time.Since(start).Seconds())
+
+	//2
+	s, sep = "", ""
+
+	start = time.Now()
+
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+
+	fmt.Printf("Join time -> %.2fs elapsed\n", time.Since(start).Seconds())
+
+	//2.5
+	s, sep = "",""
+	fmt.Println(strings.Join(os.Args[1:], " "))
+
+	//3
+	s, sep = "", ""
+	fmt.Println("Program: " + os.Args[0])
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+
+	//4
+	s, sep = "",""
+	for idx, arg := range os.Args[1:] {
+		fmt.Println("" + strconv.Itoa(idx) + ": " + arg)
+		sep = " "
+	}
+}
