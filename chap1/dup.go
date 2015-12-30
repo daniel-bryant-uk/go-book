7b8c662abb02f29fd3e47d168261b555dca53e26
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2015-12-30 12:55:51 +0000

    Complete display of filenames where duplicates are located. Add extra example test file

--- chap1/dup.go
+++ chap1/dup.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	filenames := make(map[string]string)
+	filenames := make(map[string]map[string]bool)
 
 	files := os.Args[1:]
 	if len(files) == 0 {
@@ -29,16 +29,23 @@ func main() {
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\t%s\n", n, line, filenames[line])
+			var filenameStr string
+			for key, _ := range filenames[line] {
+				filenameStr = filenameStr + " " + key
+			}
+			fmt.Printf("%d\t%s\t%s\n", n, line, filenameStr)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int, filenames map[string]string) {
+func countLines(f *os.File, counts map[string]int, filenames map[string]map[string]bool) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
-		filenames[input.Text()] = filenames[input.Text()] + " " + f.Name()
+		if filenames[input.Text()] == nil {
+			filenames[input.Text()] = make(map[string]bool)
+		}
+		filenames[input.Text()][f.Name()] = true
 	}
 	//note: ignoring potential errors from input.Err()
 }
