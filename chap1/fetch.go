72d1bac26fcd0a130464a7256308a1d9247af320
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2015-12-31 11:05:01 +0000

    Complete exercises.

--- chap1/fetch.go
+++ chap1/fetch.go
@@ -5,22 +5,26 @@ import (
 	"os"
 	"net/http"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		fmt.Fprintf(os.Stdout, "\n\nHTTP Status %v\n", resp.Status)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
 	}
 }
