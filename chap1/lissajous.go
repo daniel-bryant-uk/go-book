f301f1e802d8b9aa31e9c1fc7f0d2b52fa1654cf
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-01 12:18:40 +0000

    Add additional colors to GIF

--- chap1/lissajous.go
+++ chap1/lissajous.go
@@ -9,13 +9,15 @@ import (
 	"image/gif"
 	"image"
 	"math"
+	"fmt"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}, color.RGBA{0x00, 0x00, 0xff, 0xff}}
 
 const (
 	whiteIndex = 0 //first color in palette
 	blackIndex = 1
+	thirdIndex = 2
 )
 
 func main() {
@@ -41,7 +43,15 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles * 2 * math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t * freq + phase)
-			img.SetColorIndex(size + int(x * size + 0.5), size + int(y * size + 0.5), blackIndex)
+			var lineColorIndex uint8
+			if i % 2 == 0 {
+				lineColorIndex = blackIndex
+				fmt.Fprintf(os.Stderr, "-")
+			} else {
+				lineColorIndex = thirdIndex
+				fmt.Fprintf(os.Stderr, ".")
+			}
+			img.SetColorIndex(size + int(x * size + 0.5), size + int(y * size + 0.5), lineColorIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
