e494ee1a5693198750507a5712e42eeb49c2f947
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-05 07:36:42 +0000

    Basic muxing

--- experiments/muxing.go
+++ experiments/muxing.go
@@ -1,1 +1,23 @@
-package experiments
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/test", testHandler)
+
+	http.ListenAndServe(":8080", r)
+}
+
+func homeHandler (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome!")
+}
+
+func testHandler (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Test")
+}
