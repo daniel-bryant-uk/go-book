e04a93cbad955b3122cfc23f5af7a2c0b018a5f4
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-08 09:21:15 +0000

    Add POST handler for product

--- webserver/main.go
+++ webserver/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"fmt"
+	"strconv"
 )
 
 type Product struct {
@@ -14,6 +15,7 @@ type Product struct {
 	Email string `json:"email"`
 }
 
+var productId = 10
 var products = make([]Product, 10)
 
 //populate test users
@@ -25,7 +27,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/products", productsHandler)
-	r.HandleFunc("/product/{productId}", productHandler)
+	r.HandleFunc("/product/{productId}", productHandler).Methods("GET")
+	r.HandleFunc("/product", productCreateHandler).Methods("POST")
 
 	log.Print("Starting server...")
 	log.Fatal(http.ListenAndServe(":3000", r))
@@ -50,4 +53,30 @@ func createTestUsers() {
 	products[0] = Product{"1", "Daniel", "daniel.bryant@test.com"}
 	products[1] = Product{"2", "Ashley", "ashley@test.com"}
 	products[2] = Product{"3", "Rusty", "rusty@test.com"}
+}
+
+func productCreateHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		//Don't write or else status will be set automatically, and we
+		//get an error "http: multiple response.WriteHeader calls"
+		//fmt.Fprint(w, "Invalid data\n")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var id int
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+
+	if (name == "" || email == "") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	id = productId
+	productId++
+
+	newProduct := Product{strconv.Itoa(id), name, email}
+	products[3] = newProduct
+	w.WriteHeader(http.StatusCreated)
 }
