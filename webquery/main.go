2d4f96963e752c53f09d6ce73c0814cc735d5dca
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-12 18:20:03 +0000

    Add basic product reader application

---
+++ webquery/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
+)
+
+type Product struct {
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func main() {
+	resp, err := http.Get("wttp://localhost:3000/product/1")
+	if err != nil {
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	res := Product{}
+	json.Unmarshal(body, &res)
+	fmt.Printf("Received %v", res)
+}
