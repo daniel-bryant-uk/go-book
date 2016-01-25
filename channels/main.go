761e81b93d6d680f78d3620a221393904364f33f
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-25 10:21:16 +0000

    Add channel pipeline example

--- channels/main.go
+++ channels/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	//1
 	messages := make(chan string)
 
 	go func() {
@@ -15,7 +16,17 @@ func main() {
 	msg := <-messages
 	fmt.Println(msg)
 
+	//2
 	nextExample()
+
+	//3
+	c := wibblePostfixer("test", "test2", "test3")
+	out := blahBlahPrefixer(c)
+
+	for o := range out {
+		fmt.Println(o)
+	}
+
 }
 
 func nextExample() {
@@ -32,6 +43,28 @@ func ping(pings chan <- string, msg string) {
 }
 
 func pong(pings <- chan string, pongs chan <- string) {
-	msg := <- pings
+	msg := <-pings
 	pongs <- msg
+}
+
+func wibblePostfixer(strings ...string) <- chan string {
+	out := make(chan string)
+	go func() {
+		for _, n := range strings {
+			out <- n + " wibble"
+		}
+		close(out)
+	}()
+	return out
+}
+
+func blahBlahPrefixer(in <- chan string) <- chan string {
+	out := make(chan string)
+	go func() {
+		for n := range in {
+			out <- "blah blah " + n
+		}
+		close(out)
+	}()
+	return out
 }
