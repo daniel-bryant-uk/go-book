0a0e746b3e8bf52fe22f0cf52ddca529c0ed7477
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-01-28 23:39:08 +0000

    More complex channel experiment WIP

--- select/main.go
+++ select/main.go
@@ -3,23 +3,31 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
 func main() {
-	c1 := make(chan string)
-	c2 := make(chan string)
+	c1 := make(chan string, 1)
+	c2 := make(chan string, 1)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
 
 	go func() {
 		time.Sleep(time.Second * 1)
 		c1 <- "one"
+		wg.Done()
 	}()
 
 	go func() {
 		time.Sleep(time.Second * 2)
 		c2 <- "two"
+		wg.Done()
 	}()
 
-	for i := 0; i < 2; i++ {
+	wg.Wait()
+
+	for {
 		select {
 		case msg1 := <-c1:
 			fmt.Println("received", msg1)
